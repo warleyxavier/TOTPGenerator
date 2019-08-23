ec4f80072a5e4e67c462c806fc5fd513709e6ee8
warleyxavier <warleyandradexavier@gmail.com>
2019-08-22 21:41:34 -0300

    Inserção de validação
    Inserido recurso de validação de token

--- source/UTokenGenerator.pas
+++ source/UTokenGenerator.pas
@@ -10,16 +10,23 @@ uses
 type
   ETokenGeneratorException = class(Exception);
   EInvalidTokenDuration = class(ETokenGeneratorException);
+  EInvalidToken = class(ETokenGeneratorException);
 
   ITokenGenerator = interface
     ['{820474D2-0BBE-4454-8EE9-DBE408834FB9}']
+
+    function GetSecretKey: string;
+    procedure SetSecretKey(const Value: string);
 
     function Generate(): string; overload;
     function Generate(const DateTimeToCalculate: TDateTime): string; overload;
     function Generate(const TokenDurationInSeconds: Integer): string; overload;
     function Generate(const DateTimeToCalculate: TDateTime; const TokenDurationInSeconds: Integer): string; overload;
 
-    procedure Validate(const Token: string);
+    procedure Validate(const Token: string); overload;
+    procedure Validate(const Token: string; const DurationTime: Integer); overload;
+
+    property SecretKey: string read GetSecretKey write SetSecretKey;
 
   end;
 
@@ -27,18 +34,26 @@ type
   private
     FSecretKey: string;
 
+    function GetSecretKey: string;
+    procedure SetSecretKey(const Value: string);
+
     function GenerateToken(const DateTimeToCalculate: TDateTime; const TokenDurationInSeconds: Integer): string;
+    function IsAValidToken(const Token: string; const DateTimeToCalculate: TDateTime; const DurationTokenInSeconds: Integer): Boolean;
 
   public
 
-    constructor Create(const SecretKey: string);
+    constructor Create(const SecretKey: string); overload;
+    constructor Create; overload;
 
     function Generate(): string; overload;
     function Generate(const DateTimeToCalculate: TDateTime): string; overload;
     function Generate(const TokenDurationInSeconds: Integer): string; overload;
     function Generate(const DateTimeToCalculate: TDateTime; const TokenDurationInSeconds: Integer): string; overload;
 
-    procedure Validate(const Token: string);
+    procedure Validate(const Token: string); overload;
+    procedure Validate(const Token: string; const DurationTime: Integer); overload;
+
+    property SecretKey: string read GetSecretKey write SetSecretKey;
 
   end;
 
@@ -50,6 +65,11 @@ implementation
 constructor TTokenGenerator.Create(const SecretKey: string);
 begin
   FSecretKey := SecretKey;
+end;
+
+constructor TTokenGenerator.Create;
+begin
+  inherited;
 end;
 
 function TTokenGenerator.Generate: string;
@@ -101,9 +121,52 @@ begin
 
 end;
 
+function TTokenGenerator.GetSecretKey: string;
+begin
+  Result := FSecretKey;
+end;
+
 procedure TTokenGenerator.Validate(const Token: string);
 begin
-  // TODO -cMM: TTokenGenerator.Validate default body inserted
+  Validate(Token, DEFAULT_TOKEN_DURATION_IN_SECONDS);
+end;
+
+procedure TTokenGenerator.Validate(const Token: string; const DurationTime: Integer);
+begin
+
+  if IsAValidToken(Token, Now, DurationTime) then
+    Exit;
+
+  raise EInvalidToken.Create('Invalid token!');
+
+end;
+
+function TTokenGenerator.IsAValidToken(const Token: string; const DateTimeToCalculate: TDateTime; const DurationTokenInSeconds: Integer): Boolean;
+var
+  UnixValue, Value: Integer;
+  SimuledToken: string;
+begin
+
+  UnixValue := DateTimeToUnix(DateTimeToCalculate, False) div DurationTokenInSeconds;
+
+  for Value := UnixValue - 4 to UnixValue + 4 do
+  begin
+
+    SimuledToken := GenerateToken(Now, DurationTokenInSeconds);
+
+    if not SimuledToken.Equals(Token) then
+      continue;
+
+    Exit(True);
+
+  end;
+
+  Result := False;
+end;
+
+procedure TTokenGenerator.SetSecretKey(const Value: string);
+begin
+  FSecretKey := Value;
 end;
 
 end.
